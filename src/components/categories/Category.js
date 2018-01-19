a757c4ae055ec409efc5b3872c1a8c47956d0eeb
dalhun namgung <bnamgung@gmail.com>
2018-01-18 18:49:37 -0800

    working on

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,15 +1,18 @@
 import React from 'react';
+
 import {connect} from 'react-redux';
+
 import CategoryForm from './Category-form';
 import CategoryList from './Category-list';
 import store from '../../lib/store';
+
 import {createCategory, updateCategory, deleteCategory} from './categoryState/category-actions';
 
-
-
 class Categories extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       submitText: 'Add Category'
     }
@@ -24,22 +27,20 @@ class Categories extends React.Component {
   render() {
     return (
       <div id="categoryWrapper">
-      <CategoryForm submitText={this.state.submitText} handler={this.props.handleAddCategory}/>
+      <CategoryForm submitText={this.state.submitText} handler={this.props.handleAddCategory} />
       <CategoryList
       categories={this.props.categories}
       handleDelete={this.props.handleDeleteCategory}
       handleUpdate={this.props.handleUpdateCategory}
-      handleCategories={this.props.categories}/>
+      handleCategories={this.props.categories} />
       </div>
     )
   }
 }
 
-
 const mapStateToProps = (state) => ({
   categories: state
 });
-
 
 const mapDispatchToProps = (dispatch, getState) => ({
   handleAddCategory: category => dispatch(createCategory(category)),
@@ -47,6 +48,4 @@ const mapDispatchToProps = (dispatch, getState) => ({
   handleDeleteCategory: category => dispatch(deleteCategory(category))
 });
 
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Categories);
+export default connect(mapStateToProps,mapDispatchToProps)(Categories);
